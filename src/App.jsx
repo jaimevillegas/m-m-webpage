45fbd0a3fe1bb893ebca6d67c29db03559ce048b
Jaime Villegas <jaimevillegas296@gmail.com>
2024-01-19 11:08:49 -0500

    Add QuienesSomos component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
 import NavBar from "./NavBar";
 import Hero from "./Hero";
-import Card from "./Card";
 import HeroServices from "./HeroServices";
 import Footer from "./Footer";
 import Carousel from "./Carousel";
-
-import logo from "./assets/logo.png";
-import { IconSettings } from "@tabler/icons-react";
-import services1 from "./assets/services/services1.jpeg";
-import services2 from "./assets/services/services2.jpeg";
+import QuienesSomos from "./QuienesSomos";
 
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
-      <h1 className="text-5xl font-bold text-center my-10">Servicios</h1>
-      <div className="flex justify-center gap-10 p-5 my-10">
-        <Card
-          image={services1}
-          icon={IconSettings}
-          title={"Servicio 1"}
-          description={"lorem"}
-        />
-        <Card
-          image={services2}
-          icon={IconSettings}
-          title={"Servicio 1"}
-          description={"lorem"}
-        />
-      </div>
+      <QuienesSomos />
       <HeroServices />
       <Carousel />
       <Footer />
---
+++ src/QuienesSomos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function QuienesSomos() {
+  return <div>QuienesSomos</div>;
+}
+
+export default QuienesSomos;
