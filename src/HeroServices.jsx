78cb25a048deaf793ab5f61ad94cd90c11d59c4b
Jaime Villegas <jaimevillegas296@gmail.com>
2024-01-30 10:42:53 -0500

    Update HeroServices component and HorizontalCard styling

--- src/HeroServices.jsx
+++ src/HeroServices.jsx
@@ -1,44 +1,54 @@
 import { IconSettings } from "@tabler/icons-react";
 import HorizontalCard from "./HorizontalCard";
-import engineer from "./assets/engineer.png";
+import heroServicesImg from "./assets/heroServicesImg.jpeg";
 
 function HeroServices() {
   return (
     <div className="pb-0 pt-10 mb-0 bg-base-200 topo-pattern">
-      <h1 className="text-7xl font-bold text-palette-dark-blue text-center pt-5">
+      <h1 className="text-7xl font-bold text-palette-dark-blue text-center pt-5 mb-10">
         Servicios
       </h1>
-      <div className="px-36 flex justify-between items-center lg:flex-row py-0 w-full gap-20">
-        <img src={engineer} className="max-w-2xl rounded-lg" />
-        <div className="pl-10 w-full">
-          <HorizontalCard
-            icon={IconSettings}
-            title={"Estudios Geológicos - Geofísicos"}
-            list1={"Cartografía geológica y muestreo"}
-            list2={"Sondeos Eléctricos Verticales (SEV)"}
-            list3={"Tomografías Geoeléctricas"}
-          />
-          <HorizontalCard
-            icon={IconSettings}
-            title={"Estudios Hidrogeológicos"}
-            list1={"Modelos Hidrogeológicos Conceptuales"}
-            list2={"Estudio de Aguas Subterráneas"}
-            list3={"Diseño y Constucción de pozos de agua subterráneas"}
-          />
-          <HorizontalCard
-            icon={IconSettings}
-            title={"Estudios Geotécnicos - Civiles - Mecánicos"}
-            list1={"Estudios de Suelos"}
-            list2={"Muestreos SPT"}
-            list3={"Apiques"}
-          />
-          <HorizontalCard
-            icon={IconSettings}
-            title={"Estudios Mineros y Topográficos"}
-            list1={"Diseño y planteamiento minero"}
-            list2={"Planes de trabajo y obras (PTO)"}
-            list3={"Levantamientos topográficos altimétricos y planimétricos"}
-          />
+      <div className="px-12 gap-5 flex justify-between py-10 w-full">
+        <div className="w-1/2">
+          <img src={heroServicesImg} className="w-full rounded-lg" />
+        </div>
+        <div className="w-1/2 flex flex-col justify-between items-start gap-2">
+          <div className="w-full">
+            <HorizontalCard
+              icon={IconSettings}
+              title={"Estudios Geológicos - Geofísicos"}
+              list1={"Cartografía geológica y muestreo"}
+              list2={"Sondeos Eléctricos Verticales (SEV)"}
+              list3={"Tomografías Geoeléctricas"}
+            />
+          </div>
+          <div className="w-full">
+            <HorizontalCard
+              icon={IconSettings}
+              title={"Estudios Hidrogeológicos"}
+              list1={"Modelos Hidrogeológicos Conceptuales"}
+              list2={"Estudio de Aguas Subterráneas"}
+              list3={"Diseño y Constucción de pozos de agua subterráneas"}
+            />
+          </div>
+          <div className="w-full">
+            <HorizontalCard
+              icon={IconSettings}
+              title={"Estudios Geotécnicos - Civiles - Mecánicos"}
+              list1={"Estudios de Suelos"}
+              list2={"Muestreos SPT"}
+              list3={"Apiques"}
+            />
+          </div>
+          <div className="w-full">
+            <HorizontalCard
+              icon={IconSettings}
+              title={"Estudios Mineros y Topográficos"}
+              list1={"Diseño y planteamiento minero"}
+              list2={"Planes de trabajo y obras (PTO)"}
+              list3={"Levantamientos topográficos altimétricos y planimétricos"}
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/HorizontalCard.jsx
+++ src/HorizontalCard.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable react/prop-types */
 function HorizontalCard({ icon: Icon, title, list1, list2, list3 }) {
   return (
-    <div className="card card-side bg-palette-sky-blue shadow-xl my-5 transition hover:shadow-2xl">
-      <div className="flex items-center justify-center pl-12 pr-8">
+    <div className="card card-side bg-palette-sky-blue shadow-xl  transition hover:shadow-2xl">
+      <div className="flex items-center justify-center pl-10 pr-8">
         <div className="bg-palette-orange p-4 rounded-full">
-          <Icon size={64} style={{ color: "black" }} />
+          <Icon size={48} style={{ color: "black" }} />
         </div>
       </div>
       <div className="card-body ">
-        <h2 className="card-title text-palette-dark-blue text-2xl">{title}</h2>
+        <h2 className="card-title text-palette-dark-blue text-xl">{title}</h2>
         <ul className="list-disc">
-          <li className="text-lg py-1">{list1}</li>
-          <li className="text-lg py-1">{list2}</li>
-          <li className="text-lg py-1">{list3}</li>
+          <li className="text-md ">{list1}</li>
+          <li className="text-md ">{list2}</li>
+          <li className="text-md ">{list3}</li>
         </ul>
       </div>
     </div>
