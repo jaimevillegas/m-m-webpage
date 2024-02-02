e71d6ced8f58f02ce03c16f9ceb21a7006fc76d5
Jaime Villegas <jaimevillegas296@gmail.com>
2024-02-02 15:49:16 -0500

    Finish Mobile version for Galeria

--- src/Galeria.jsx
+++ src/Galeria.jsx
@@ -7,11 +7,11 @@ function Galeria() {
     <>
       <NavBar />
       <div className="pb-0 pt-10 mb-0 bg-base-200 gallery-pattern">
-        <div className="card bg-white mx-32 mt-32 p-10 shadow-xl">
-          <h1 className="text-poppins font-semibold text-palette-dark-blue text-6xl text-center mb-10">
+        <div className="card bg-white lg:mx-32 lg:mt-32 lg:p-10 mx-4 px-4 mt-24 shadow-xl">
+          <h1 className="text-poppins font-semibold text-palette-dark-blue text-5xl pt-8 lg:text-6xl text-center mb-10">
             GALERÍA
           </h1>
-          <div className="px-12">
+          <div className="px-2 lg:px-12">
             <MyGallery />
           </div>
         </div>
