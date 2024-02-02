82a1d45222b9b07c056eb67d734d1786cb4a1073
Jaime Villegas <jaimevillegas296@gmail.com>
2024-02-02 12:28:35 -0500

    Finish mobile version for Hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -10,11 +10,11 @@ function Hero() {
     >
       <div className="hero-overlay bg-opacity-85"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="px-32">
-          <h1 className="my-16 text-7xl font-bold text-palette-orange text-center">
+        <div className="px-6 lg:px-32">
+          <h1 className="mt-28 lg:my-16 text-3xl lg:text-7xl font-bold text-palette-orange text-center">
             M&M GEOINGENIERÍA
           </h1>
-          <p className="my-12 text-palette-orange  text-3xl">
+          <p className="my-6 lg:my-12 text-palette-orange text-lg lg:text-3xl">
             Soluciones en el sector de la Ingeniería, Consultoría e
             Hidrocarburos
           </p>
