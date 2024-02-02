b45e7a7e537971c3b8dd03e6b2563c1cb95fd1ef
Jaime Villegas <jaimevillegas296@gmail.com>
2024-02-02 12:20:49 -0500

    Finish mobile version for NavBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="navbar bg-palette-sky-blue fixed shadow z-10">
-      <div className="navbar-start">
+    <div className="navbar bg-palette-sky-blue fixed shadow z-10 w-full">
+      <div className="navbar-start w-full">
         <div className="dropdown">
           <div
             tabIndex={0}
@@ -28,29 +28,44 @@ function NavBar() {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-6 shadow bg-base-100 rounded-box w-60"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-6 shadow bg-palette-sky-blue rounded-box w-72"
           >
             <li>
-              <a>INICIO</a>
+              <Link to="/">
+                <p className="text-xl">INICIO</p>
+              </Link>
             </li>
             <li>
-              <a>INGENIERÍA Y DISEÑO</a>
+              <Link to="/ingenieria">
+                <p className="text-xl">INGENIERÍA Y DISEÑO</p>
+              </Link>
             </li>
             <li>
-              <a>CONSULTORÍA</a>
+              <Link to="/consultoria">
+                <p className="text-xl">CONSULTORÍA</p>
+              </Link>
             </li>
             <li>
-              <a>NUESTROS CLIENTES</a>
+              <Link to="/galeria">
+                <p className="text-xl">GALERÍA</p>
+              </Link>
             </li>
             <li>
-              <a>CONTÁCTENOS</a>
+              <Link to="/clientes-proyectos">
+                <p className="text-xl">CLIENTES Y PROYECTOS</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/contacto">
+                <p className="text-xl">CONTÁCTENOS</p>
+              </Link>
             </li>
           </ul>
         </div>
         {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
-        <a href="/">
+        <Link to="/">
           <img src={logo} alt="logo" className="h-20" />
-        </a>
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
@@ -81,7 +96,7 @@ function NavBar() {
           </li>
         </ul>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end hidden lg:block">
         <Link
           to="/contacto"
           className="btn btn-md bg-palette-blue text-white hover:bg-palette-dark-blue"
