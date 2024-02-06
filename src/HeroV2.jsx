f060564fb1927a741579b169d70c3899073d0e7c
Jaime Villegas <jaimevillegas296@gmail.com>
2024-02-06 14:29:07 -0500

    Add animate.css and aos dependencies, update HeroV2 component

--- src/HeroV2.jsx
+++ src/HeroV2.jsx
@@ -1,11 +1,11 @@
-import hero2 from "./assets/hero2.jpeg";
+import hero2 from "./assets/hero3.jpg";
 
 function HeroV2() {
   return (
     <div className=" justify-center items-center   pt-24">
       <div className="w-full h-screen  topo-pattern flex items-center justify-center">
         <div className="px-6 lg:px-32">
-          <h1 className="my-12 lg:my-24 text-3xl lg:text-5xl  font-bold text-palette-orange text-center">
+          <h1 className="text-3xl lg:text-5xl  font-bold text-palette-orange text-center">
             BIENVENIDO A
           </h1>
           <h1 className="my-12 lg:my-24 text-4xl lg:text-7xl font-bold text-palette-orange text-center">
@@ -22,9 +22,11 @@ function HeroV2() {
           </div>
         </div>
       </div>
-      {/* <div className="w-1/2 h-screen"> */}
-      <img src={hero2} className="object-cover  h-screen flex-none" alt="" />
-      {/* </div> */}
+      <img
+        src={hero2}
+        className="object-cover w-full h-screen flex-none"
+        alt=""
+      />
     </div>
   );
 }
